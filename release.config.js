61a08d67466b39822eb465612f176fc58277dc46
Mikael Fehrm <mikael.fehrm@live.se>
2023-08-21 11:40:40 +0200

    feat: test
    BREAKING_CHANGE:TEST release config js bortkommenterad

--- release.config.js
+++ release.config.js
@@ -1,18 +1,18 @@
-const config = {
-  branches: ["master"],
-  plugins: [
-    "@semantic-release/commit-analyzer", // Increments the version for us before 'passing' the commit to next plugins,
-    "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/git", // git configs, takes assets, stages commit and pushes to repo. 
-      {
-        assets: ["package.json, dist/*", "dist/*.js.map"],
-        message: // edit commit message/release note and skipping CI to prevent an endless loop
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
-    "@semantic-release/github" // creates the release and adds releasenotes
-  ],
-};
+// const config = {
+//   branches: ["master"],
+//   plugins: [
+//     "@semantic-release/commit-analyzer", // Increments the version for us before 'passing' the commit to next plugins,
+//     "@semantic-release/release-notes-generator",
+//     [
+//       "@semantic-release/git", // git configs, takes assets, stages commit and pushes to repo. 
+//       {
+//         assets: ["package.json, dist/*", "dist/*.js.map"],
+//         message: // edit commit message/release note and skipping CI to prevent an endless loop
+//           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+//       },
+//     ],
+//     "@semantic-release/github" // creates the release and adds releasenotes
+//   ],
+// };
 
-module.exports = config
+// module.exports = config
